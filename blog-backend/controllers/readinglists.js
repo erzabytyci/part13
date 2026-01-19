735e702c97dc9aac34cd561d9fd73ace9f9d3b18
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2026-01-19 23:44:30 +0200

    Exercise 13.22.

--- blog-backend/controllers/readinglists.js
+++ blog-backend/controllers/readinglists.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { ReadingList } = require('../models')
+const { userExtractor } = require('../util/auth')
 
 router.post('/', async (req, res) => {
     const { blogId, userId } = req.body
@@ -11,5 +12,33 @@ router.post('/', async (req, res) => {
 
     res.status(201).json(reading)
 })
+
+router.put('/:id', userExtractor, async (req, res) => {
+    const reading = await ReadingList.findByPk(req.params.id)
+
+    if (!reading) {
+        const err = new Error('reading list entry not found')
+        err.name = 'NotFoundError'
+        throw err
+    }
+
+    if (reading.userId !== req.user.id) {
+        const err = new Error('not allowed')
+        err.name = 'ForbiddenError'
+        throw err
+    }
+
+    if (req.body.read === undefined) {
+        const err = new Error('read missing')
+        err.name = 'BadRequestError'
+        throw err
+    }
+
+    reading.read = req.body.read
+    await reading.save()
+
+    res.json(reading)
+})
+
 
 module.exports = router
